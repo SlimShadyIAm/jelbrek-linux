77a5b8deded0a873316151dff122ff520ace0be1
Aamir Farooq <aamir@farooq.xyz>
2020-04-13 18:33:32 +0200

    Push all files

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-jelbrek-linux
+This guide will use KDE's `spectacle` as the screenshot application. Install that, along with the following dependencies:
+
+-   `jq`
+-   `xsel`
+-   `curl`
+-   `notify-send`
+
+Once you have installed the dependencies, you need to set your Jelbrek token up as an environment variable. You can do this by:
+
+1. Creating a file under `/etc/profile.d/jelbrek.sh`
+2. As the contents, write:
+    > export JELBREK_TOKEN="yourtokenhere"
+3. Save the file, then log out and back in
+
+If you don't set up the token or the dependencies properly, you will get an error message.
+
+Next, create a file under `/usr/bin/jelbrek` and add the contents of the `jelbrek.sh` file from this repo.
+
+Set this file as executable.
+
+> chmod +x /usr/bin/jelbrek
+
+Create file `/usr/share/applications/jelbrek.desktop` and write contents of `jelbrek.desktop` from this repo
+
+Now, when you create a screenshot with spectacle, hit the `Export` button and you should see `jelbrek` in the list. Click that and the image will be uploaded. Once done, the link should be copied to your clipboard.
+
+When you screenshot, an image is made under `~/Pictures` and it is deleted automatically 60 seconds after the screenshot upload attempt has been made.
+
+I haven't done a whole lot of error checking. If you run into issues contact me at SlimShadyIAm#9999 on Discord.
---
+++ jelbrek.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Check dependencies
+if [ "$JELBREK_TOKEN" = "" ] ;then
+    notify-send "You haven't set your token. Please set your token up as an environment variable with the name JELBREK_TOKEN."
+    sleep 60
+    rm $1
+    exit 1
+fi
+
+if [ "$(which jq)" = "" ] ;then
+    notify-send "This script requires dependency jq to upload to parse the response. Please resolve."
+    sleep 60
+    rm $1
+    exit 1
+fi
+
+if [ "$(which curl)" = "" ] ;then
+    notify-send "This script requires dependency curl. Please resolve."
+    sleep 60
+    rm $1
+    exit 1
+fi
+
+if [ "$(which xsel)" = "" ] ;then
+    notify-send "This script requires dependency xsel. Please resolve."
+    sleep 60
+    rm $1
+    exit 1
+fi
+
+# POST the image to jelbrek's endpoint. store response temporarily
+response=$(curl --form "x=@$(echo $1)"  -H "Content-Type: multipart/form-data" -H "authorization: $JELBREK_TOKEN" http://jelbrek.icu/upload)
+# parse response to see if request was successful
+success=$(echo $response | jq -r '.success')
+# parse repsonse, try to get the URL for the uploaded image
+path=$(echo $response | jq -r '.url')
+
+if [ "$success" == "true" ]; then
+    # copy path to clipboard
+    echo $path | xsel -bi
+    notify-send $path
+    sleep 60
+    rm $1
+    exit 1
+else
+    notify-send "Error uploading!"
+    sleep 60
+    rm $1
+    exit 1
+fi
